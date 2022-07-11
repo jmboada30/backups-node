1fefca4fd115bc4382571e2b2403898e1d93aa26
Joel M. Boada V <jmboada30@gmail.com>
2022-07-11 04:28:22 -0400

    ahora usamos type module y add pcloud-sdk

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
-const cron = require('node-cron');
-require('dotenv').config();
-
-const path = require('path');
+import child_process from 'child_process';
+import util from 'util';
+import cron from 'node-cron';
+import 'dotenv/config';
 
 console.log('Starting backup...');
+console.log('running a task every minute');
 
+const exec = util.promisify(child_process.exec);
 const CONTAINER_NAME = process.env.CONTAINER_NAME;
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
 cron.schedule('*/1 * * * *', () => {
-  console.log('running a task every minute');
   backupDockerMariaDB();
 });
 
 async function backupDockerMariaDB() {
   const DATE = getDate();
-  const ARCHIVE_PATH = path.join(
-    __dirname,
-    'backups',
-    `${DB_NAME}-${DATE}.sql.gz`
+  const { pathname: ARCHIVE_PATH } = new URL(
+    `backups/${DB_NAME}-${DATE}.sql.gz`,
+    import.meta.url
   );
 
   const command = `docker exec ${CONTAINER_NAME} mysqldump -u ${USERNAME} -p${PASSWORD} --events --routines --triggers ${DB_NAME} | gzip -c > ${ARCHIVE_PATH}`;
 
   const { stderr } = await exec(command);
 
-  if (stderr) return console.error('stderr:', stderr);
+  if (stderr) return console.error('stderr:', Buffer.from(stderr).toString());
 
   console.log('Backup created:', ARCHIVE_PATH);
 }
