e9c3a546fb4ae3e5c7e778ed2dab4bffc8aa51c4
Joel M. Boada V <jmboada30@gmail.com>
2022-07-11 06:23:09 -0400

    ahora eliminamos los backups viejos

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 
-cron.schedule('*/1 * * * *', () => {
+cron.schedule(process.env.CRON_TIME, () => {
+  removeBackupOlds();
   backupDockerMariaDB();
 });
 
@@ -23,7 +24,7 @@ async function backupDockerMariaDB() {
     import.meta.url
   );
 
-  const command = `docker exec ${CONTAINER_NAME} mysqldump -u ${USERNAME} -p${PASSWORD} --events --routines --triggers ${DB_NAME} | gzip -c > ${ARCHIVE_PATH}`;
+  const command = `sudo docker exec ${CONTAINER_NAME} mysqldump -u ${USERNAME} -p${PASSWORD} --events --routines --triggers ${DB_NAME} | gzip -c > ${ARCHIVE_PATH}`;
 
   const { stderr } = await exec(command);
 
@@ -40,4 +41,18 @@ function getDate() {
   const hour = date.getHours();
   const minute = date.getMinutes();
   return `${year}-${month}-${day}_${hour}:${minute}`;
+}
+
+async function removeBackupOlds() {
+
+  // Find permite buscar archivos en un directorio
+  // -type f indica que solo busque archivos
+  // -atime +1 indica que busque archivos que hayan sido accedidos hace un dia
+  const command = `find ./backups -type f -atime +3 -delete`;
+
+  const { stderr } = await exec(command);
+
+  if (stderr) return console.error('stderr:', Buffer.from(stderr).toString());
+
+  console.log('Fueron eliminados los archivos antiguos');
 }
